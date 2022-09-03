9f2008e34b9383fd2801783ab5ad7695dd1856b3
IIdentity <107352642+IIdentity@users.noreply.github.com>
2022-09-03 04:42:02 +0200

    Update Http.lua

--- Libraries/Http.lua
+++ Libraries/Http.lua
@@ -1,32 +1,7 @@
 local HttpService = game:GetService("HttpService")
-local http_request = (syn and syn.request) or (http and http.request) or http_request
+local http_request = syn.request or http.request or http_request
 local Http = {}
-function Http:decode(stuff)
-    local stuff = stuff or {}
-    HttpService:JSONDecode(stuff.Stuff)
+function Http:request(a)
+	return http_request(a)
 end
-function Http:encode(stuff)
-    local stuff = stuff or {}
-    HttpService:JSONEncode(stuff.Stuff)
-end
-function Http:load(url)
-    local url = url or {}
-    loadstring(game:HttpGet(url.Url))()
-end
-function Http:request(url)
-    local url = url or {}
-    return http_request({
-        Url = url.Url,
-        Method = "GET"
-    })
-end
-function Http:post(info)
-    local info = info or {}
-    local url = info.Url
-    local headers = info.Headers
-    return http_request({
-        Url = url,
-        Headers = headers,
-        Method = "POST"
-    })
-end
+return Http
